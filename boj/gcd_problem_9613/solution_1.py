fadbc6b286ef7d4c6f2b7ab2ee6b835fc1aee6c8
joongwon <anjoongwon517@gmail.com>
2025-08-09 12:03:31 +0900

    gcd 합

---
+++ boj/gcd_problem_9613/solution_1.py
@@ -0,0 +1,19 @@
+import sys 
+from math import gcd
+from itertools import combinations
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    arr = list(map(int, input().split()))
+    N = arr[0] # 배열 크기
+    nums = arr[1:] # 실제 숫자 리스트
+
+    total = 0
+    for i in range(N):
+        for j in range(i+1, N):
+            total = total + gcd(nums[i], nums[j])
+    
+    print(total)
